f1beb0c21fff4b9ff6bc57a85a30185bb89c9bd3
max <muller.max06@gmail.com>
2025-01-27 15:28:47 +0200

    Add getters and setters for cells in the board

--- SudokuBoard.cs
+++ SudokuBoard.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,49 @@ namespace MaxSudoku
 {
     internal class SudokuBoard
     {
+        private int boardSize;
+        private int[,] board;
+
+        public SudokuBoard(int boardSize)
+        {
+            this.boardSize = boardSize;
+        }
+
+        public void FillBoard(string sudokuString)
+        {
+            int row = 0;
+            int col = 0;
+            foreach (char c in sudokuString)
+            {
+                board[row, col] = (byte)(c - '0');
+                col++;
+                if (col == boardSize)
+                {
+                    col = 0;
+                    row++;
+                }
+            }
+        }
+
+        public int GetCell(int row, int col)
+        {
+            return board[row, col];
+        }
+
+        public void SetCell(int row, int col, int value)
+        {
+            board[row, col] = value;
+        }
+
+
+
+
+
+
+
+
+
+
     }
+
 }
