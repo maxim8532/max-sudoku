bdebbc44bbaa348f142ca3e02697714d1a4dbdab
max <muller.max06@gmail.com>
2025-02-10 00:47:29 +0200

    Add properties, getters and setters

---
+++ BoardGame.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaxSudoku
+{
+    internal abstract class BoardGame
+    {
+        protected int size;
+        public int Size => size;
+
+        protected BoardGame(int size)
+        {
+            this.size = size;
+        }
+
+        public abstract void FillBoard(string boardData);
+        public virtual bool IsFull() => false;
+        public virtual int GetCell(int row, int col) => 0;
+        public virtual void SetCell(int row, int col, int value) { }
+    }
+}
