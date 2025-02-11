9b0edfccad1cbc4fb5df310d58481c03f2e29ab1
max <muller.max06@gmail.com>
2025-02-12 01:45:35 +0200

    Add undo moves logic for heuristic moves

--- Solver/MovesManager.cs
+++ Solver/MovesManager.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MaxSudoku.Board;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -16,9 +17,43 @@ namespace MaxSudoku.Solver
         /// <summary>
         /// Records a move.
         /// </summary>
+        /// <param name="move">A move that the solver did.</param>
         public void RecordMove(Move move)
         {
             moveStack.Push(move);
+        }
+
+        /// <summary>
+        /// Returns the current count of recorded moves.
+        /// </summary>
+        public int Count => moveStack.Count;
+
+        /// <summary>
+        /// Undos moves until the stack is restored to a given return point of the backtracking.
+        /// </summary>
+        /// <param name="returnPoint">The point which the backtracking return to (the recursive call)</param>
+        /// <param name="board">The Sudoku board.</param>
+        /// <param name="maskManager">The mask manager to update the mask for the cells being reverted.</param>
+        public void UndoMoves(int returnPoint, SudokuBoard board, MaskManager maskManager)
+        {
+            while (moveStack.Count > returnPoint)
+            {
+                Move move = moveStack.Pop();
+
+                // Restore board cell
+                board.SetCell(move.Row, move.Col, move.OldValue);
+
+                // Undo the mask update for the move we are reverting.
+                if (move.NewValue != 0)
+                {
+                    maskManager.UpdateMasks(move.Row, move.Col, move.NewValue, isPlacing: false);
+                }
+                // If the previous value was non-zero, restore it in the mask.
+                if (move.OldValue != 0)
+                {
+                    maskManager.UpdateMasks(move.Row, move.Col, move.OldValue, isPlacing: true);
+                }
+            }
         }
     }
 }
