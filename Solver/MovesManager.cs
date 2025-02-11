3fe739b33343e05d01d31d3db7345d9804c1433d
max <muller.max06@gmail.com>
2025-02-12 01:32:15 +0200

    Add moves-manager class to record moves

---
+++ Solver/MovesManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaxSudoku.Solver
+{
+    /// <summary>
+    /// Records moves (both guesses and heuristic placements) so that they can be undone on backtracking.
+    /// </summary>
+    public class MovesManager
+    {
+        private readonly Stack<Move> moveStack = new Stack<Move>();
+
+        /// <summary>
+        /// Records a move.
+        /// </summary>
+        public void RecordMove(Move move)
+        {
+            moveStack.Push(move);
+        }
+    }
+}
