fdcd2b2a26754f709477a7a4d2f1f766a99dd5e7
max <muller.max06@gmail.com>
2025-02-18 02:54:03 +0200

    Add solver utility class

--- Solver/SudokuSolver.cs
+++ Solver/SudokuSolver.cs
@@ -1,6 +1,8 @@
 ﻿using MaxSudoku.Board;
 using MaxSudoku.CustomExceptions;
 using MaxSudoku.Solver.Heuristics;
+using static MaxSudoku.Solver.SudokuSolverUtils;
+using MaxSudoku.Solver;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -32,7 +34,7 @@ namespace MaxSudoku.Solver
             // Validate board size
             blockSize = (int)Math.Sqrt(boardSize);
             if (blockSize * blockSize != boardSize)
-                throw new InvalidInputException("Board size must be a perfect square (for example: 9x9, 16x16).");
+                throw new InvalidInputException("Board size must be a perfect square (for example: 4x4, 9x9, 16x16).");
 
             maskManager = new MaskManager(boardSize);
             maskManager.UpdateFromBoard(board);  // Initializes board masks.
@@ -42,20 +44,19 @@ namespace MaxSudoku.Solver
             heuristics.AddHeuristic(new HiddenSinglesHeuristic(board, maskManager, movesManager));
         }
 
-
         /// <summary>
         /// Attempts to solve the Sudoku puzzle using backtracking.
         /// </summary>
         /// <returns>True if a valid solution is found, false otherwise.</returns>
         public bool Solve()
         {
-            ApplyHeuristics();
+            ApplyHeuristics(heuristics);
             try
             {
                 bool result = SolveRecursive();
                 if (result)
                 {
-                    ValidateSolution();
+                    ValidateSolution(board);
                 }
                 return result;
             }
@@ -64,17 +65,16 @@ namespace MaxSudoku.Solver
                 return false;
             }
         }
-        
 
         /// <summary>
         /// Recursive solving method that uses backtracking with bit manipulations.
         /// </summary>
         private bool SolveRecursive()
         {
-            if (!EveryCellHasAvailableDigitsCheck())
+            if (!EveryCellHasAvailableDigitsCheck(board, maskManager, boardSize))
                 return false;
 
-            var (row, col, foundEmpty) = FindMinimumEmptyCell();
+            var (row, col, foundEmpty) = FindMinimumEmptyCell(board, maskManager, boardSize);
             if (!foundEmpty)
                 return true;
 
@@ -83,21 +83,21 @@ namespace MaxSudoku.Solver
 
             while (availableDigitis != 0)
             {
-                // Extract least significant 1 bit.
+                /* Extract least significant 1 bit. */
                 int bit = availableDigitis & -availableDigitis;
 
-                // Clear the least significant 1 bit from mask.
+                /* Clear the least significant 1 bit from mask.*/
                 availableDigitis &= availableDigitis - 1;
 
-                // Counts 0 bits from right till the first 1 bit (trailing zeros),
-                // Then adds 1 to get the digit.
+                /* Counts 0 bits from right till the first 1 bit (trailing zeros),
+                   Then adds 1 to get the digit. */
                 int digit = BitOperations.TrailingZeroCount(bit) + 1;
 
                 movesManager.RecordMove(new Move(row, col, 0, digit));
-                PlaceDigit(row, col, digit);
+                PlaceDigit(board, maskManager, row, col, digit);
 
                 /* Try heuristic moves again. */
-                ApplyHeuristics();
+                ApplyHeuristics(heuristics);
 
                 if (SolveRecursive())
                     return true;
@@ -106,105 +106,6 @@ namespace MaxSudoku.Solver
                 movesManager.UndoMoves(checkpoint, board, maskManager);
             }
             return false;
-
-        }
-
-        /// <summary>
-        /// Checks if every cell has atleast 1 option,
-        /// thus reducing the solving time by pruning
-        /// the unsolvable branch.
-        /// </summary>
-        /// <returns>True if every cell has atleast 1 option.</returns>
-        private bool EveryCellHasAvailableDigitsCheck()
-        {
-            for (int i = 0; i < boardSize; i++)
-            {
-                for (int j = 0; j < boardSize; j++)
-                {
-                    if (board.GetCell(i, j) == 0)
-                    {
-                        if (maskManager.GetAvailableDigits(i, j) == 0)
-                            return false;
-                    }
-                }
-            }
-            return true;
-        }
-
-        /// <summary>
-        /// Applies all of the added heuristics in order.
-        /// </summary>
-        private void ApplyHeuristics()
-        {
-            bool heuristicProgress;
-            do
-            {
-                heuristicProgress = heuristics.ApplyAll();
-            }
-            while (heuristicProgress);
-        }
-
-
-        /// <summary>
-        /// Places a digit on the board and updates masks.
-        /// </summary>
-        private void PlaceDigit(int row, int col, int digit)
-        {
-            board.SetCell(row, col, digit);
-            maskManager.UpdateMasks(row, col, digit, isPlacing: true);
-        }
-
-        /// <summary>
-        /// Finds the empty cell with the minimum value
-        /// of available digits in the board.
-        /// </summary>
-        /// <returns>Returns an empty cell if found (tuple). Otherwise, false</returns>
-        private (int row, int col, bool foundEmpty) FindMinimumEmptyCell()
-        {
-            int minCount = int.MaxValue;
-            int targetRow = -1;
-            int targetCol = -1;
-            bool foundEmpty = false;
-
-            for (int row = 0; row < boardSize; row++)
-            {
-                for (int col = 0; col < boardSize; col++)
-                {
-                    if (board.GetCell(row, col) == 0)
-                    {
-                        foundEmpty = true;
-                        int available = maskManager.GetAvailableDigits(row, col);
-                        int count = BitOperations.PopCount((uint)available);
-
-                        /* If this cell has fewer options than the current minimum, update.*/
-                        if (count < minCount)
-                        {
-                            minCount = count;
-                            targetRow = row;
-                            targetCol = col;
-                        }
-
-                        /* Exit Early if a cell has only one option.*/
-                        if (minCount == 1)
-                        {
-                            return (targetRow, targetCol, true);
-                        }
-                    }
-                }
-            }
-
-            return (targetRow, targetCol, foundEmpty);
-        }
-
-        /// <summary>
-        /// Validates the final solution using the board's validation rules.
-        /// </summary>
-        private void ValidateSolution()
-        {
-            if (board is SudokuBoard sudokuBoard)
-            {
-                sudokuBoard.ValidateBoard();
-            }
         }
     }
 }
---
+++ Solver/SudokuSolverUtils.cs
@@ -0,0 +1,113 @@
+﻿using MaxSudoku.Board;
+using MaxSudoku.Solver.Heuristics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaxSudoku.Solver
+{
+    public class SudokuSolverUtils
+    {
+
+        /// <summary>
+        /// Checks if every cell has atleast 1 option,
+        /// thus reducing the solving time by pruning
+        /// the unsolvable branch.
+        /// </summary>
+        /// <returns>True if every cell has atleast 1 option.</returns>
+        public static bool EveryCellHasAvailableDigitsCheck(SudokuBoard board, MaskManager maskManager, int boardSize)
+        {
+            for (int i = 0; i < boardSize; i++)
+            {
+                for (int j = 0; j < boardSize; j++)
+                {
+                    if (board.GetCell(i, j) == 0)
+                    {
+                        if (maskManager.GetAvailableDigits(i, j) == 0)
+                            return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// Applies all of the added heuristics in order.
+        /// </summary>
+        public static void ApplyHeuristics(SudokuHeuristics heuristics)
+        {
+            bool heuristicProgress;
+            do
+            {
+                heuristicProgress = heuristics.ApplyAll();
+            }
+            while (heuristicProgress);
+        }
+
+        /// <summary>
+        /// Places a digit on the board and updates masks.
+        /// </summary>
+        public static void PlaceDigit(SudokuBoard board, MaskManager maskManager, int row, int col, int digit)
+        {
+            board.SetCell(row, col, digit);
+            maskManager.UpdateMasks(row, col, digit, isPlacing: true);
+        }
+
+        /// <summary>
+        /// Finds the empty cell with the minimum value
+        /// of available digits in the board.
+        /// </summary>
+        /// <returns>Returns an empty cell if found (tuple). Otherwise, false</returns>
+        public static (int row, int col, bool foundEmpty) FindMinimumEmptyCell(SudokuBoard board, MaskManager maskManager, int boardSize)
+        {
+            {
+                int minCount = int.MaxValue;
+                int targetRow = -1;
+                int targetCol = -1;
+                bool foundEmpty = false;
+
+                for (int row = 0; row < boardSize; row++)
+                {
+                    for (int col = 0; col < boardSize; col++)
+                    {
+                        if (board.GetCell(row, col) == 0)
+                        {
+                            foundEmpty = true;
+                            int available = maskManager.GetAvailableDigits(row, col);
+                            int count = BitOperations.PopCount((uint)available);
+
+                            /* If this cell has fewer options than the current minimum, update.*/
+                            if (count < minCount)
+                            {
+                                minCount = count;
+                                targetRow = row;
+                                targetCol = col;
+                            }
+
+                            /* Exit Early if a cell has only one option.*/
+                            if (minCount == 1)
+                            {
+                                return (targetRow, targetCol, true);
+                            }
+                        }
+                    }
+                }
+                return (targetRow, targetCol, foundEmpty);
+            }
+        }
+
+        /// <summary>
+        /// Validates the final solution using the board's validation rules.
+        /// </summary>
+        public static void ValidateSolution(SudokuBoard board)
+        {
+            if (board is SudokuBoard sudokuBoard)
+            {
+                sudokuBoard.ValidateBoard();
+            }
+        }
+    }
+}
