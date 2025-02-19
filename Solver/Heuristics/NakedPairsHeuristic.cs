e8c9829bb64aea1614f43dbc5cb6fa4a0814ef2b
max <muller.max06@gmail.com>
2025-02-19 15:59:16 +0200

    Add properties and constructor

--- Solver/Heuristics/NakedPairsHeuristic.cs
+++ Solver/Heuristics/NakedPairsHeuristic.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MaxSudoku.Board;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,23 @@ using System.Threading.Tasks;
 
 namespace MaxSudoku.Solver.Heuristics
 {
-    public class NakedPairsHeuristic
+    public class NakedPairsHeuristic : Heuristic
     {
+        private readonly List<(int row, int col)>[] rowGroups;
+        private readonly List<(int row, int col)>[] columnGroups;
+        private readonly List<(int row, int col)>[] blockGroups;
+
+        public NakedPairsHeuristic(SudokuBoard board, MaskManager maskManager, MovesManager movesManager)
+            : base(board, maskManager, movesManager)
+        {
+            rowGroups = new List<(int row, int col)>[boardSize];
+            columnGroups = new List<(int row, int col)>[boardSize];
+            blockGroups = new List<(int row, int col)>[boardSize];
+        }
+
+        public override bool Apply()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
